11c83e4583f08549a73e1b70c43a944b7a8fe28b
errorscode <altair1379@gmail.com>
2017-03-31 18:01:37 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -254,7 +254,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {136888679},
+    sudo_users = {335375038,363277816},
     admin_users = {},
     disabled_channels = {}
   }
